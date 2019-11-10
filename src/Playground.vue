068c65c303782b2ed45ab2a420feeb7221f62bf9
Callum Axon <callumaxon@gmail.com>
2019-11-10 20:10:59 +0000

    amend old references to headline

--- src/Playground.vue
+++ src/Playground.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app" class="font-display bg-grey-base">
     <vuk-headline type="super">Super Heading</vuk-headline>
-    <vuk-headline type="heading-1">Super Heading</vuk-headline>
-    <vuk-headline type="heading-2">Super Heading</vuk-headline>
-    <vuk-headline type="heading-3">Super Heading</vuk-headline>
-    <vuk-headline type="heading-4">Super Heading</vuk-headline>
+    <vuk-headline type="heading-1">Heading 1</vuk-headline>
+    <vuk-headline type="heading-2">Heading 2</vuk-headline>
+    <vuk-headline type="heading-3">Heading 3</vuk-headline>
+    <vuk-headline type="heading-4">Heading 4</vuk-headline>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import VukHeadline from '@/components/VukHeading/VukHeading.vue';
+import VukHeading from '@/components/VukHeading/VukHeading.vue';
 export default Vue.extend({
   name: 'playground',
 
   components: {
-    VukHeadline
+    VukHeadline: VukHeading
   }
 });
 </script>
